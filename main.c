4a9c4546de6fe68b1c2890b818610c17a22ffb04
anthonyjarjar <anthonyjarjar@users.noreply.github.com>
2023-03-06 01:02:33 -0800

    Implemented memoizatoin expression must have a constant valueC/C++(28)

--- main.c
+++ main.c
@@ -2,76 +2,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 1000
+
 #define RECURSIVE 'r'
 
-void printLongArray(long *arr, int arr_length);
+//! TO DO: GIT PUSH!!!, and fix syntax, such as unecessary {} on if and loop statements
 
-bool recInSequence(long lookingFor, int currSeqLen);
-long recamanRecursively(int term_number);
+void printArray(int *arr, int arrLength);
+
+bool recInSequence(int *cache, long lookingFor, int currSeqLen);
+long recamanRecursively(int *cache, int termNum);
 
 int main(int argc, char *argv[])
 {
 
-    if ((argc < 3) || (argv[1][0] != RECURSIVE) ||
-        (atoi(argv[2]) < 1))
+    // Preforms input validation
+    if ((argc < 2) || (atoi(argv[1]) < 1))
     {
-        fprintf(stderr, "Usage: %s <r> <pos num>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <pos num>\n", argv[0]);
         return 1;
     }
 
-    if (argv[1][0] == RECURSIVE)
+    // Declares sequence length, and creates an array ready to store the sequence
+    int seqLen = atoi(argv[1]);
+
+    int *cache = malloc((sizeof(int)) * (seqLen));
+    for (int idx = 0; idx < seqLen; ++idx)
+        cache[idx] = -1;
+
+    // Preforms the loop to create the sequence
+    for (int termNum = 0; termNum < seqLen; termNum++)
     {
-        int seqLen = atoi(argv[2]);
-        long *recamanSeq = malloc(sizeof(long) * seqLen);
-
-        for (int termNum = 0; termNum < seqLen; termNum++)
-        {
-            printf("."); // Temporary loading visuilization
-            recamanSeq[termNum] = recamanRecursively(termNum);
-        }
-
-        printf("\n"); // Using for format
-        printLongArray(recamanSeq, seqLen);
-
-        free(recamanSeq);
+        printf("."); // Temporary loading visuilization
+        recamanRecursively(cache, termNum);
     }
+
+    printf("\n"); // Using for format
+    // Prints the sequence
+    printArray(cache, seqLen);
+
+    free(cache);
 
     return 0;
 }
 
-void printLongArray(long *arr, int arr_length)
+void printArray(int *arr, int arrLength)
 {
-    while (arr_length--)
-    {
-        printf("%ld ", *arr++);
-    }
+    while (arrLength--)
+        printf("%d ", *arr++);
+
     printf("\n");
 }
 
-bool recInSequence(long lookingFor, int currSeqLen)
+bool recInSequence(int *cache, long lookingFor, int currSeqLen)
 {
     if (currSeqLen == 0)
-        return lookingFor == recamanRecursively(0);
+        return lookingFor == 0;
 
-    if (lookingFor == recamanRecursively(currSeqLen))
+    if (lookingFor == cache[currSeqLen])
         return true;
 
-    return recInSequence(lookingFor, currSeqLen - 1);
+    return recInSequence(cache, lookingFor, currSeqLen - 1);
 }
 
-long recamanRecursively(int termNum)
+long recamanRecursively(int *cache, int termNum)
 {
     if (termNum == 0)
-        return 0;
+    {
+        cache[termNum] = 0;
+        return cache[termNum];
+    }
 
-    long prevTerm = recamanRecursively(termNum - 1);
+    if (cache[termNum] == -1)
+    {
+        long prevTerm = cache[termNum - 1];
+        long potentialTerm = prevTerm - termNum;
+        bool validTerm = ((potentialTerm > 0) && (!recInSequence(cache, potentialTerm, termNum - 1)));
 
-    long potentialTerm = prevTerm - termNum;
-    bool validTerm =
-        potentialTerm > 0 && !recInSequence(potentialTerm, termNum - 1);
+        if (!validTerm)
+        {
+            potentialTerm = prevTerm + termNum;
+            cache[termNum] = potentialTerm;
+            return cache[termNum];
+        }
 
-    if (!validTerm)
-        potentialTerm = prevTerm + termNum;
-
-    return potentialTerm;
+        cache[termNum] = potentialTerm;
+        return cache[termNum];
+    }
 }
