b864e232a25753e77fd8dd0efead6a4fd6d6cf47
anthonyjarjar <anthjar203@gmail.com>
2024-05-13 22:14:56 -0700

    Finished the visiulization and cache sequence

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define CACHE_SIZE 100
 
 void printArray(int *arr, int arrLength);
-
 bool recInSequence(int *cache, long lookingFor, int currSeqLen);
 long recamanRecursively(int *cache, int termNum);
+void addToCache(int *cache, int value, int *cacheSize);
+void drawRecaman(SDL_Renderer *renderer, int *sequence, int seqLen);
 
 int main(int argc, char *argv[])
 {
-
     if ((argc < 2) || (atoi(argv[1]) < 1))
     {
         fprintf(stderr, "Usage: %s <pos num>\n", argv[0]);
@@ -18,16 +23,82 @@ int main(int argc, char *argv[])
 
     int seqLen = atoi(argv[1]);
 
-    int *cache = malloc((sizeof(int)) * (seqLen));
-    for (int idx = 0; idx < seqLen; ++idx)
+    int *cache = malloc((sizeof(int)) * CACHE_SIZE);
+    if (cache == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(-1);
+    }
+    for (int idx = 0; idx < CACHE_SIZE; ++idx)
         cache[idx] = -1;
 
-    for (int termNum = 0; termNum < seqLen; termNum++)
-        recamanRecursively(cache, termNum);
+    int *sequence = malloc((sizeof(int)) * seqLen);
+    if (sequence == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(-1);
+    }
 
-    printArray(cache, seqLen);
+    for (int termNum = 0; termNum < seqLen; termNum++)
+        sequence[termNum] = recamanRecursively(cache, termNum);
+
+    printArray(sequence, seqLen);
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+        free(cache);
+        free(sequence);
+        exit(1);
+    }
+
+    SDL_Window *win = SDL_CreateWindow("Recaman Sequence Visualization", 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (win == NULL)
+    {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        free(cache);
+        free(sequence);
+        exit(1);
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL)
+    {
+        SDL_DestroyWindow(win);
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        free(cache);
+        free(sequence);
+        exit(1);
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+
+    drawRecaman(renderer, sequence, seqLen);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 
     free(cache);
+    free(sequence);
 
     return 0;
 }
@@ -43,13 +114,14 @@ void printArray(int *arr, int arrLength)
 
 bool recInSequence(int *cache, long lookingFor, int currSeqLen)
 {
-    if (currSeqLen == 0)
-        return lookingFor == 0;
-
-    if (lookingFor == cache[currSeqLen])
-        return true;
-
-    return recInSequence(cache, lookingFor, currSeqLen - 1);
+    for (int i = 0; i < currSeqLen; i++)
+    {
+        if (cache[i] == lookingFor)
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 long recamanRecursively(int *cache, int termNum)
@@ -60,20 +132,53 @@ long recamanRecursively(int *cache, int termNum)
         return cache[termNum];
     }
 
-    if (cache[termNum] == -1)
+    long prevTerm = cache[termNum - 1];
+    long potentialTerm = prevTerm - termNum;
+    bool validTerm = (potentialTerm > 0) && (!recInSequence(cache, potentialTerm, termNum));
+
+    if (!validTerm)
     {
-        long prevTerm = cache[termNum - 1];
-        long potentialTerm = prevTerm - termNum;
-        bool validTerm = ((potentialTerm > 0) && (!recInSequence(cache, potentialTerm, termNum - 1)));
+        potentialTerm = prevTerm + termNum;
+    }
 
-        if (!validTerm)
+    addToCache(cache, potentialTerm, &termNum);
+    return potentialTerm;
+}
+
+void addToCache(int *cache, int value, int *cacheSize)
+{
+    if (*cacheSize < CACHE_SIZE)
+    {
+        cache[*cacheSize] = value;
+        (*cacheSize)++;
+    }
+    else
+    {
+        for (int i = 1; i < CACHE_SIZE; i++)
         {
-            potentialTerm = prevTerm + termNum;
-            cache[termNum] = potentialTerm;
-            return cache[termNum];
+            cache[i - 1] = cache[i];
         }
+        cache[CACHE_SIZE - 1] = value;
+    }
+}
 
-        cache[termNum] = potentialTerm;
-        return cache[termNum];
+void drawRecaman(SDL_Renderer *renderer, int *sequence, int seqLen)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    int x0 = WINDOW_WIDTH / 2;
+    int y0 = WINDOW_HEIGHT / 2;
+    int x1 = x0;
+    int y1 = y0;
+    int radius = 20;
+
+    for (int i = 1; i < seqLen; i++)
+    {
+        int x2 = x0 + (sequence[i] - sequence[i - 1]) * radius;
+        int y2 = y0;
+
+        SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+        x1 = x2;
+        y1 = y2;
     }
 }
